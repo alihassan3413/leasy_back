d11901d4306f18f69ca6c0248b40ac28620708a9
adilanwar <adilanwar0318@gmail.com>
2026-04-10 21:00:51 +0330

    Setup the configuration of the autoimport component

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,21 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'src/types/auto-imports.d.ts',
+    }),
+    Components({
+      dirs: ['src/components'],
+      dts: 'src/types/components.d.ts',
+    }),
   ],
 
   resolve: {
