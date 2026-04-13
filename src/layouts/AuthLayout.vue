2a60eb4b1a73f8ee0185d51af21b08cf7e5e1d8f
Adil2463 <adilanwar0318@gmail.com>
2026-04-13 19:39:42 +0500

    Complete the login and registration responsiveness and also add the register and login page toggle in the card

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import leasybackLogo from "@/assets/logo/leasyback-logo.svg";
+import { useRoute } from "vue-router";
+const route = useRoute();
 </script>
 
 <template>
-  <div class="relative flex min-h-screen overflow-hidden">
+  <div class="relative flex md:min-h-screen overflow-hidden">
     <div
       class="absolute left-0 top-0 z-0 h-full w-full md:w-1/2"
       style="background-color: #fafafa"
@@ -23,11 +25,38 @@ import leasybackLogo from "@/assets/logo/leasyback-logo.svg";
       style="left: -19.44vw; top: -11.85vw; width: 103.68vw; height: auto"
     />
 
-    <div class="relative flex w-full md:w-1/2 items-center justify-center px-4 md:px-6 py-1 md:py-12">
-      <div class="relative z-20 w-full max-w-[420px]">
+    <div class="relative flex w-full md:w-1/2 items-center justify-center  md:px-6  md:py-12">
+      <div class="relative z-20 w-full max-w-105">
+        
         <div
           class="rounded-[10px] bg-white border px-9 pb-9 border-green-gray shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
         >
+         <!-- Tabs -->
+          <div class="flex mb-2 md:hidden">
+
+            <!-- Login Tab -->
+            <RouterLink
+              to="/auth/login"
+              class="flex-1 text-center py-3 font-semibold transition"
+              :class="route.path.includes('login')
+                ? 'bg-white text-custom-orange'
+                : 'bg-gray-200 text-black'"
+            >
+              Einloggen
+            </RouterLink>
+
+            <!-- Register Tab -->
+            <RouterLink
+              to="/auth/register"
+              class="flex-1 text-center py-3 font-semibold transition"
+              :class="route.path.includes('register')
+                ? 'bg-white text-custom-orange'
+                : 'bg-gray-200 text-black'"
+            >
+              Registrieren
+            </RouterLink>
+
+          </div>
           <RouterView />
         </div>
       </div>
@@ -35,19 +64,19 @@ import leasybackLogo from "@/assets/logo/leasyback-logo.svg";
 
     <!-- Right side — branding panel (hidden on mobile) -->
     <div
-      class="relative z-0 hidden w-1/2 items-center justify-center overflow-hidden bg-primary px-12 lg:flex"
+      class="relative z-0 hidden md:flex w-1/2 md:items-center md:justify-center overflow-hidden bg-primary px-12 lg:flex"
     >
       <div
-        class="relative z-20 w-full max-w-[565px] flex flex-col items-center text-left"
+        class="relative z-20 w-full max-w-141.25 flex flex-col items-center text-left"
       >
         <img
           :src="leasybackLogo"
           alt="LeasyBack"
-          class="h-auto w-full max-w-[220px] md:max-w-[300px] lg:max-w-[400px] xl:max-w-[565px]"
+          class="h-auto w-full max-w-55 md:max-w-75 lg:max-w-100 xl:max-w-141.25"
         />
 
         <p
-          class="mt-8 xl:mt-10 2xl:mt-12 w-full max-w-[386px] xl:max-w-[454px] 2xl:max-w-[533px] text-[20px] font-normal leading-normal text-white lg:text-[24px] xl:text-[32px]"
+          class="mt-8 xl:mt-10 2xl:mt-12 w-full max-w-96.5 xl:max-w-113.5 2xl:max-w-133.25 text-[20px] font-normal leading-normal text-white lg:text-[24px] xl:text-[32px]"
         >
           Sicher dir jetzt dein kostenloses Minderwert-Gutachten, erhalte günstige
           <span class="text-custom-orange">Reparaturangebote</span>
