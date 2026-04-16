9c46a469a751bbbaf805a72f3e353d99ebb245fd
Adil2463 <adilanwar0318@gmail.com>
2026-04-16 20:06:28 +0500

    fix the not using the Register layout in the RegisterCompanyview

--- src/components/company/Register.vue
+++ src/components/company/Register.vue
@@ -4,7 +4,9 @@ import FormTextField from '@/components/ui/form/FormTextField.vue'
 
 <template>
   <div class="w-full rounded bg-white mt-2.5">
+
     <div class="flex flex-col items-start px-5 py-5">
+
       <h2 class="text-primary text-[20px] font-bold leading-normal not-italic">
         Registrierung
       </h2>
@@ -12,7 +14,9 @@ import FormTextField from '@/components/ui/form/FormTextField.vue'
       <div class="w-full h-px bg-green-gray mb-4"></div>
 
       <div class="w-full space-y-4">
+
         <div class="grid grid-cols-2 gap-4">
+
           <FormTextField
             name="company.firmenname"
             label="Firmenname (lt. HGB/Gewerbeeintrag)"
@@ -28,6 +32,7 @@ import FormTextField from '@/components/ui/form/FormTextField.vue'
         </div>
 
         <div class="grid grid-cols-[1fr_100px_1fr] gap-3">
+
           <FormTextField
             name="company.strasse"
             label="Straße"
@@ -47,6 +52,7 @@ import FormTextField from '@/components/ui/form/FormTextField.vue'
         </div>
 
         <div class="grid grid-cols-[100px_1fr_1fr] gap-3 items-end">
+
           <FormTextField
             name="company.plz"
             label="PLZ"
@@ -60,16 +66,21 @@ import FormTextField from '@/components/ui/form/FormTextField.vue'
           />
 
           <div class="flex flex-col gap-0.5">
+
             <span class="text-sm font-bold leading-normal not-italic text-black">
               Land
             </span>
+
             <span class="text-sm text-black font-normal leading-normal not-italic h-8 flex items-center">
               Deutschland
             </span>
+
           </div>
+
         </div>
 
         <div class="flex flex-col gap-0.5 mt-2.5">
+
           <span class="text-black text-sm font-bold leading-normal not-italic">
             Laden Sie Ihr Logo hoch
           </span>
@@ -87,15 +98,24 @@ import FormTextField from '@/components/ui/form/FormTextField.vue'
                 <span class="text-custom-green text-sm font-bold leading-normal not-italic underline">
                   durchsuchen
                 </span>
+
               </span>
+
               <br />
+
               <span class="text-custom-black text-[12px] tracking-[1px] font-normal leading-normal not-italic block">
                 PDF, JPG oder PNG • 8MB max
               </span>
+
             </p>
+
           </label>
+
         </div>
+
       </div>
+
     </div>
+    
   </div>
 </template>
